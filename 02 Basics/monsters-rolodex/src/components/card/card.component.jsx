a6da7f940b294d4e2db645f3759ebd4ecf4c27c1
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2022-11-16 06:31:44 +0200

    monsters rolodex

--- 02 Basics/monsters-rolodex/src/components/card/card.component.jsx
+++ 02 Basics/monsters-rolodex/src/components/card/card.component.jsx
@@ -1,25 +1,19 @@
-import { Component } from "react";
 import "./card.styles.css";
 
-class Card extends Component {
-   render() {
-      const { id, firstName, image, email } = this.props.monster;
-      return (
-         <div className="card" key={id}>
-            <img
-               alt={`monster ${firstName}`}
-               src={`${image}`}
-               onError={(e) =>
-                  (e.currentTarget.src =
-                     "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=")
-               }
-               width="100"
-            />
-            <h1>{firstName}</h1>
-            <p>{email}</p>
-         </div>
-      );
-   }
-}
+const Card = ({ monster: { id, firstName, image, email } }) => (
+   <div className="card" key={id}>
+      <img
+         alt={`monster ${firstName}`}
+         src={`${image}`}
+         onError={(e) =>
+            (e.currentTarget.src =
+               "https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=")
+         }
+         width="100"
+      />
+      <h1>{firstName}</h1>
+      <p>{email}</p>
+   </div>
+);
 
 export default Card;
