a91e1fb3df549467095771d4daf9b26cc1643c42
M-Ramadan <mr.uiux.dev@gmail.com>
2022-11-21 16:52:09 +0200

    continue

--- 03 crwn-clothing/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ 03 crwn-clothing/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,13 @@
+import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-   return <></>;
+   return (
+      <div className="cart-dropdown-container">
+         <div className="cart-items" />
+         <Button>GO TO CHECKOUT</Button>
+      </div>
+   );
 };
 
 export default CartDropdown;
--- 03 crwn-clothing/crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ 03 crwn-clothing/crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/auth/firebase.utils";
@@ -34,6 +35,7 @@ const Navigation = () => {
                )}
                <CartIcon />
             </div>
+            <CartDropdown />
          </div>
          <Outlet />
       </>
